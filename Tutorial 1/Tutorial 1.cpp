636b9b394ccc50822474c09f58828e8b5f75b90c
jmesmarston <benjameswileman@gmail.com>
2022-03-10 18:29:05 +0000

    week 1 commit

--- Tutorial 1/Tutorial 1.cpp
+++ Tutorial 1/Tutorial 1.cpp
@@ -12,12 +12,12 @@ void print_help() {
 	std::cerr << "  -h : print this message" << std::endl;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
 	//Part 1 - handle command line options such as device selection, verbosity, etc.
 	int platform_id = 0;
 	int device_id = 0;
 
-	for (int i = 1; i < argc; i++)	{
+	for (int i = 1; i < argc; i++) {
 		if ((strcmp(argv[i], "-p") == 0) && (i < (argc - 1))) { platform_id = atoi(argv[++i]); }
 		else if ((strcmp(argv[i], "-d") == 0) && (i < (argc - 1))) { device_id = atoi(argv[++i]); }
 		else if (strcmp(argv[i], "-l") == 0) { std::cout << ListPlatformsDevices() << std::endl; }
@@ -34,7 +34,7 @@ int main(int argc, char **argv) {
 		std::cout << "Running on " << GetPlatformName(platform_id) << ", " << GetDeviceName(platform_id, device_id) << std::endl;
 
 		//create a queue to which we will push commands for the device
-		cl::CommandQueue queue(context);
+		cl::CommandQueue queue(context, CL_QUEUE_PROFILING_ENABLE);
 
 		//2.2 Load & build the device code
 		cl::Program::Sources sources;
@@ -58,9 +58,11 @@ int main(int argc, char **argv) {
 		//host - input
 		std::vector<int> A = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; //C++11 allows this type of initialisation
 		std::vector<int> B = { 0, 1, 2, 0, 1, 2, 0, 1, 2, 0 };
-		
+		//std::vector<int> A(100000000);
+		//std::vector<int> B(100000000);
+
 		size_t vector_elements = A.size();//number of elements
-		size_t vector_size = A.size()*sizeof(int);//size in bytes
+		size_t vector_size = A.size() * sizeof(int);//size in bytes
 
 		//host - output
 		std::vector<int> C(vector_elements);
@@ -72,17 +74,47 @@ int main(int argc, char **argv) {
 
 		//Part 4 - device operations
 
+		// Tut 1 
+		//Enable profiling for the queue:
+		//cl::CommandQueue queue(context, CL_QUEUE_PROFILING_ENABLE);
+		//Create an eventand attach it to a queue command responsible for the kernel launch :
+		cl::Event prof_event;
+
+
 		//4.1 Copy arrays A and B to device memory
+		//cl::Event A_event;
+		//cl::Event B_event;
+		//queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, vector_size, &A[0], NULL, &A_event);
+		//queue.enqueueWriteBuffer(buffer_B, CL_TRUE, 0, vector_size, &B[0], NULL, &B_event);
 		queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, vector_size, &A[0]);
 		queue.enqueueWriteBuffer(buffer_B, CL_TRUE, 0, vector_size, &B[0]);
 
 		//4.2 Setup and execute the kernel (i.e. device code)
 		cl::Kernel kernel_add = cl::Kernel(program, "add");
-		kernel_add.setArg(0, buffer_A);
+		kernel_add.setArg(0, buffer_C);
 		kernel_add.setArg(1, buffer_B);
 		kernel_add.setArg(2, buffer_C);
 
-		queue.enqueueNDRangeKernel(kernel_add, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange);
+
+		cl::Kernel kernel_mult = cl::Kernel(program, "mult");
+		kernel_mult.setArg(0, buffer_A);
+		kernel_mult.setArg(1, buffer_B);
+		kernel_mult.setArg(2, buffer_C);
+
+		cl::Kernel kernel_mult_add = cl::Kernel(program, "mult_add");
+		kernel_mult_add.setArg(0, buffer_A);
+		kernel_mult_add.setArg(1, buffer_B);
+		kernel_mult_add.setArg(2, buffer_C);
+
+		//clGetDeviceInfo(CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE)
+
+		//queue.enqueueNDRangeKernel(kernel_add, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange, NULL, &prof_event);
+		//queue.enqueueNDRangeKernel(kernel_add, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange, NULL, &prof_event);
+		//queue.enqueueNDRangeKernel(kernel_mult, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange);
+		//queue.enqueueNDRangeKernel(kernel_add, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange);
+
+		queue.enqueueNDRangeKernel(kernel_mult_add, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange);
+
 
 		//4.3 Copy the result from device to host
 		queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, vector_size, &C[0]);
@@ -90,6 +122,17 @@ int main(int argc, char **argv) {
 		std::cout << "A = " << A << std::endl;
 		std::cout << "B = " << B << std::endl;
 		std::cout << "C = " << C << std::endl;
+		//std::cout << "Kernel execution time [ns]:" <<
+		//	prof_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() -
+		//	prof_event.getProfilingInfo<CL_PROFILING_COMMAND_START>() << std::endl;
+		//std::cout << GetFullProfilingInfo(prof_event, ProfilingResolution::PROF_US)
+		//	<< std::endl;
+		//std::cout << GetFullProfilingInfo(A_event, ProfilingResolution::PROF_US)
+		//	<< std::endl;
+
+		//cl::Event sum_event = A_event + B_event;
+		//std::cout << GetFullProfilingInfo(sum_event, ProfilingResolution::PROF_US)
+		//	<< std::endl;
 	}
 	catch (cl::Error err) {
 		std::cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << std::endl;
--- Tutorial 1/kernels/my_kernels.cl
+++ Tutorial 1/kernels/my_kernels.cl
@@ -1,13 +1,27 @@
 //a simple OpenCL kernel which adds two vectors A and B together into a third vector C
 kernel void add(global const int* A, global const int* B, global int* C) {
-	int id = get_global_id(0);
+	int id = get_global_id(0); //Each launch gets a separate and unique ID which is obtained by get_global_id(0) function.
 	C[id] = A[id] + B[id];
+}
+
+// Multiple the vectors together
+kernel void mult(global const int* A, global const int* B, global int* C) {
+	int id = get_global_id(0);
+	//C[id] = A[id] + B[id];
+	C[id] = A[id] * B[id];
+}
+
+// mult and add vectors together in the form: C = A * B + B
+kernel void mult_add(global const int* A, global const int* B, global int* C) {
+	int id = get_global_id(0);
+	//C[id] = A[id] + B[id];
+	C[id] = A[id] * B[id] + B[id];
 }
 
 //a simple smoothing kernel averaging values in a local window (radius 1)
 kernel void avg_filter(global const int* A, global int* B) {
 	int id = get_global_id(0);
-	B[id] = (A[id - 1] + A[id] + A[id + 1])/3;
+	B[id] = (A[id - 1] + A[id] + A[id + 1]) / 3;
 }
 
 //a simple 2D kernel
@@ -16,9 +30,9 @@ kernel void add2D(global const int* A, global const int* B, global int* C) {
 	int y = get_global_id(1);
 	int width = get_global_size(0);
 	int height = get_global_size(1);
-	int id = x + y*width;
+	int id = x + y * width;
 
 	printf("id = %d x = %d y = %d w = %d h = %d\n", id, x, y, width, height);
 
-	C[id]= A[id]+ B[id];
+	C[id] = A[id] + B[id];
 }
